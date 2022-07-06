760c488bef1142f0b66c35fd9136c90a22f9a728
Joy <799840822@qq.com>
2022-07-06 23:31:53 +0800

    feat: 引入 pca-code.json 数据源

--- src/components/ChooseArea/src/index.vue
+++ src/components/ChooseArea/src/index.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
-    省市区选择
+    <el-select v-model="province">
+      <el-option value="1"></el-option>
+    </el-select>
+    <el-select v-model="city" style="margin: 0 10px;">
+      <el-option value="2"></el-option>
+    </el-select>
+    <el-select v-model="area">
+      <el-option value="3"></el-option>
+    </el-select>
   </div>
 </template>
 <script setup lang="ts">
-
+import { ref } from 'vue';
+import allArea from '../lib/pca-code.json';
+console.log(allArea);
+let province = ref<string>('')
+let city = ref<string>('')
+let area = ref<string>('')
 </script>
 <style lang="">
   
--- src/views/chooseArea/index.vue
+++ src/views/chooseArea/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     省市区选择
+    <m-choose-area></m-choose-area>
   </div>
 </template>
 <script setup lang="ts">
-
+import mChooseArea from '@/components/ChooseArea/src/index.vue'
 </script>
 <style lang="">
   
