14e6b0cc5da6c22dfedbc3dd6506c99b93a4d131
Joy <799840822@qq.com>
2023-02-01 19:33:26 +0800

    perf: 允许自定义导航内容

--- src/components/Menu/src/index.vue
+++ src/components/Menu/src/index.vue
@@ -1,20 +1,20 @@
 <template>
   <el-menu :default-active="defaultActive" :router="router" v-bind="$attrs">
-    <template v-for="(item, index) in data" :key="index">
-      <el-menu-item v-if="!item.children || !item.children.length" :index="item.index">
-        <component v-if="item.icon" :is="`el-icon-${transToKeyBase(item.icon)}`"></component>
-        <span>{{ item.name }}</span>
+    <template v-for="(item, i) in data" :key="i">
+      <el-menu-item v-if="!item[children] || !item[children].length" :index="item.index">
+        <component v-if="item[icon]" :is="`el-icon-${transToKeyBase(item[icon])}`"></component>
+        <span>{{ item[name] }}</span>
       </el-menu-item>
 
       <el-sub-menu v-if="item.children && item.children.length" :index="item.index">
         <template #title>
-          <component v-if="item.icon" :is="`el-icon-${transToKeyBase(item.icon)}`"></component>
-          <span>{{ item.name }}</span>
+          <component v-if="item[icon]" :is="`el-icon-${transToKeyBase(item[icon])}`"></component>
+          <span>{{ item[name] }}</span>
         </template>
         <template v-for="(childrenItem, childrenIndex) in item.children" :key="childrenIndex">
           <el-menu-item :index="childrenItem.index">
-            <component v-if="childrenItem.icon" :is="`el-icon-${transToKeyBase(childrenItem.icon)}`"></component>
-            <span>{{ childrenItem.name }}</span>
+            <component v-if="childrenItem[icon]" :is="`el-icon-${transToKeyBase(childrenItem[icon])}`"></component>
+            <span>{{ childrenItem[name] }}</span>
           </el-menu-item>
         </template>
       </el-sub-menu>
@@ -28,7 +28,7 @@ import { transToKeyBase } from '../../../utils'
 
 let props = defineProps({
   data: {
-    type: Array as PropType<MenuItem[]>,
+    type: Array as PropType<any[]>,
     required: true
   },
   defaultActive: {
@@ -38,6 +38,22 @@ let props = defineProps({
   router: {
     type: Boolean,
     default: false
+  },
+  name: {
+    type: String,
+    default: 'name'
+  },
+  index: {
+    type: String,
+    default: 'index'
+  },
+  icon: {
+    type: String,
+    default: 'icon'
+  },
+  children: {
+    type: String,
+    default: 'children'
   }
 })
 
--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -1,28 +1,28 @@
 <template>
   <div style="display: flex">
-    <m-menu :data="data1" style="width: 200px" defaultActive="1"></m-menu>
+    <m-menu :data="data1" style="width: 200px" defaultActive="1" :name="'a'"></m-menu>
     <m-infinite-menu :data="data2" style="width: 200px" defaultActive="1" active-text-color="red" popper-effect="dark"></m-infinite-menu>
   </div>
 </template>
 <script lang="ts" setup>
 let data1 = [
   {
-    name: '导航1',
+    a: '导航1',
     index: '1',
     icon: 'document'
   },
   {
-    name: '导航2',
+    a: '导航2',
     index: '2',
     icon: 'document'
   },
   {
-    name: '导航3',
+    a: '导航3',
     index: '3',
     icon: 'document',
     children: [
       {
-        name: '导航3-1',
+        a: '导航3-1',
         index: '3.1',
         icon: 'document'
       }
